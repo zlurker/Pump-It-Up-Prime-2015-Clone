83c90e9eb53c6c14ae873cbbb0be11252bc75c1a
zlurker <tallspin_97@hotmail.com>
2016-09-23 22:26:51 +0800

    <Slightly further testing with BPM change formula>
    -It works. Testing it with Milky Way Galaxy D18 by next commit.

--- Pump It Up Prime 2015/Assets/Scripts/StepchartMover.cs
+++ Pump It Up Prime 2015/Assets/Scripts/StepchartMover.cs
@@ -20,6 +20,7 @@ public class StepchartMover : MonoBehaviour {
 
     bool test;
     bool test2;
+    bool test3;
 
     float currentBeat;
     
@@ -29,25 +30,26 @@ public class StepchartMover : MonoBehaviour {
         song.pitch = rush;
         endTime = (endBpm / bpm) * 60;
         song.Play();
-        Debug.Log(endTime);
 	}
 	
 	void Update () {
         cRealTime = Time.realtimeSinceStartup - offset;
         transform.position = new Vector2(2, ((cRealTime - dOffset) / endTime) * totalDist);
 
-        if (cRealTime > (7.5f/rush)&& !test){
-            ChangeBpm(60, beatNo);
+        /*if (cRealTime > (7.5f/rush)&& !test){
+            ChangeBpm(1600, beatNo);
             test = true;
         }
 
-        if (cRealTime > (17.5f/rush) && !test2) {
+        if (cRealTime > ((7.5f + 0.375f)/rush) && !test2) {
             ChangeBpm(160, beatNo2);
             test2 = true;
         }
-        //if (cRealTime > ((beatNo/160) * 60) + ((10/100) * 60)) {
-          //  ChangeBpm(160, beatNo2);
-        //}
+
+        if (cRealTime > ((7.5f + 0.375f +7.5f) / rush) && !test3) {
+            ChangeBpm(160*2, 50);
+            test3 = true;
+        }*/
     }
 
     void ChangeBpm(float bpmToChange, float currentBeat) {
